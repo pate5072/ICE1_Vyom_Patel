27d134a81240a31bd394b8df91e09f1fc429d782
pate5072 <122803511+pate5072@users.noreply.github.com>
2023-01-26 03:17:18 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -48,6 +48,20 @@ public class CardTrick {
 
 //About Me
         private static void printInfo() {
+        System.out.println("My name is Vyom Patel")
+        System.out.println("I am 18 Years Old")
+        System.out.println("I am a domestic student")
+        System.out.println("My Program is SDNE")
+                
+        System.out.println("My career ambitions:  Game Engineer or Software Engineer");
+        System.out.println("-- Continue Programming and Designing Games");
+        System.out.println("-- Have a semester with no violations of academic integrity!");
+
+        System.out.println("My hobbies:");
+        System.out.println("-- Drawing Illustrations");
+        System.out.println("-- Designing Maps and programming");
+        System.out.println("-- Learning  LUA and C++ on the side");
+        System.out.println("-- Gaming");
     }
 
 }
